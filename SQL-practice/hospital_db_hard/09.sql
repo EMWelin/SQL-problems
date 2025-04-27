7c64bdac6c250036b0082cff3bbeb5e2995e1b41
EMWelin <117637537+EMWelin@users.noreply.github.com>
2025-04-27 11:18:45 +0200

    Update 09.sql

--- SQL-practice/hospital_db_hard/09.sql
+++ SQL-practice/hospital_db_hard/09.sql
@@ -2,13 +2,11 @@
 
 -- Hard problems from the Hospital problem set.
 
-/*
-We need to LAG a calculated column 'admission_day'. This is a CTE problem as I can't
-create a calculated column and then use it for more calculations in the same SELECT.
-
-The part to remember in this problem was that I can select admission_date without refering
-the admissions table because it was referenced in my CTE.
+/* 
+For each day, display the total amount of admissions on that day.
+Display the amount changed from the previous date.
 */
+
 
 WITH admission_count AS (
     SELECT
@@ -23,3 +21,11 @@ SELECT
     admission_day,
     admission_day - LAG(admission_day) OVER (ORDER BY admission_date) AS admission_count_change
 FROM admission_count;
+
+/* My comment:
+We need to LAG a calculated column 'admission_day'. This is a CTE problem as I can't
+create a calculated column and then use it for more calculations in the same SELECT.
+
+The part to remember in this problem was that I can select admission_date without refering
+the admissions table because it was referenced in my CTE.
+*/
